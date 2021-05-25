dfec57ae2f751ecd1063198b07bf04ac1de8c4f4
Josue Valencia <josu@Josues-MacBook-Pro.local>
2021-05-25 12:32:34 -0500

    atfirewall startup

--- src/pages/soluciones/at-firewall.js
+++ src/pages/soluciones/at-firewall.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import Layout from '../../components/layouts/main-layout'
-import Seo from '../../components/seo'
+import React from "react"
+import Layout from "../../components/layouts/main-layout"
+import Seo from "../../components/seo"
+import sslVideo from "../../videos/alpha_ssl_video3.mp4"
 
 function atfirewall() {
-    return (
-        <Layout>
-            <Seo title="AT Firewall" />
-            <section className="simpleSection">
-                <div className="sectionWrapper">
-                    <h1>Esta es la página del AT Firewall</h1>
-                </div>
-            </section>
-        </Layout>
-    )
+  return (
+    <Layout bgDark={true}>
+      <Seo title="AT Firewall" />
+      <section className="heroSection">
+        
+        <div className="overlayContent">
+          <h1>Evita los ataques externos.</h1>
+          <p>
+            Brindamos una solución integral de seguridad perimetral. Bajo el
+            análisis de nuestros expertos, aplicarán políticas correctas para la
+            prevención de ataques a sus sistemas.
+          </p>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
 export default atfirewall
