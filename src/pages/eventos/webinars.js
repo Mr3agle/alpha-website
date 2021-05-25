3ef003496b2d37e3bba1f842c5569d5b28c2ddd8
Mr.Eagle <mreagle.design@gmail.com>
2021-05-25 01:06:50 -0500

    atdrone

--- src/pages/eventos/webinars.js
+++ src/pages/eventos/webinars.js
@@ -2,14 +2,17 @@ import * as React from "react"
 import Layout from "../../components/layouts/main-layout"
 import Seo from "../../components/seo"
 
-function WebinarsPage(){
-
-    return(
-        <Layout>
-            <Seo title="Webinars"/>
-            <h1 className="mt-3">This is webinars</h1>
-        </Layout>
-    )
-
+function WebinarsPage() {
+  return (
+    <Layout>
+      <Seo title="Webinars" />
+      <div className="simpleSection">
+        <div className="simpleWrapper">
+          <h1>Esta es la página de Webinars</h1>
+        </div>
+      </div>
+    </Layout>
+  )
 }
+
 export default WebinarsPage
