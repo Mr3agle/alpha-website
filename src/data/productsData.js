97816b712173a9047f50a4e504822d6b8e52cdc3
Mr.Eagle <mreagle.design@gmail.com>
2021-06-14 13:56:14 -0500

    added widget styles

--- src/data/productsData.js
+++ src/data/productsData.js
@@ -6,55 +6,61 @@ import atMailing from "../images/ATMailing-pkg.png"
 import atProxy from "../images/ATProxy-pkg.png"
 import atDrone from "../images/ATdron2.png"
 
-
 export const products = [
-    {
-        title: "AT Firewall",
-        description: "Evita los ataques externos con una solución integral de seguridad perimetral.",
-        img: atFirewall,
-        path:"/soluciones/at-firewall",
-        cta: "Ver más"
-    },
-    {
-        title: "AT Mail Inspector",
-        description: "Controla y filtra grandes cantidades de correo basura de forma sencilla.",
-        img: atInspector,
-        path:"/soluciones/at-inspector",
-        cta: "Ver más"
-    },
-    {
-        title: "AT Proxy",
-        description: "Controla la navegación con un filtro de contenido inteligente y optimiza tus recursos.",
-        img: atProxy,
-        path:"/soluciones/at-proxy",
-        cta: "Ver más"
-    },
-    {
-        title: "AT Mailing",
-        description: "Envía más de <b>1.000.000 </b> de correos diarios sin caer en listas negras.",
-        img: atMailing,
-        path:"/soluciones/at-mailing",
-        cta: "Ver más"
-    },
-    {
-        title: "AT Hotspot",
-        description: "Tickets de navegación para clientes e invitados en una zona segura de acceso inalámbrico.",
-        img: atHotspot,
-        path:"/soluciones/at-hotspot",
-        cta: "Ver más"
-    },
-    {
-        title: "AT Logic",
-        description: "La solución óptima para el balanceo de carga, alto rendimiento y alta disponibilidad.",
-        img: atLogic,
-        path:"/soluciones/at-logic",
-        cta: "Ver más"
-    },
-    {
-        title: "AT Drone",
-        description: "Drones de alto rendimiento pensados para todo tipo de proyecto táctico o multimedia.",
-        img: atDrone,
-        path:"/soluciones/at-firewall",
-        cta: "Ver más"
-    }
+  {
+    title: "AT Firewall",
+    description:
+      "Evita los ataques externos con una solución integral de seguridad perimetral.",
+    img: atFirewall,
+    path: "/soluciones/at-firewall",
+    cta: "Ver más",
+  },
+  {
+    title: "AT Mail Inspector",
+    description:
+      "Controla y filtra grandes cantidades de correo basura de forma sencilla.",
+    img: atInspector,
+    path: "/soluciones/at-inspector",
+    cta: "Ver más",
+  },
+  {
+    title: "AT Proxy",
+    description:
+      "Controla la navegación con un filtro de contenido inteligente y optimiza tus recursos.",
+    img: atProxy,
+    path: "/soluciones/at-proxy",
+    cta: "Ver más",
+  },
+  {
+    title: "AT Mailing",
+    description:
+      "Envía más de <b>1.000.000 </b> de correos diarios sin caer en listas negras.",
+    img: atMailing,
+    path: "/soluciones/at-mailing",
+    cta: "Ver más",
+  },
+  {
+    title: "AT Hotspot",
+    description:
+      "Tickets de navegación para clientes e invitados en una zona segura de acceso inalámbrico.",
+    img: atHotspot,
+    path: "/soluciones/at-hotspot",
+    cta: "Ver más",
+  },
+  {
+    title: "AT Logic",
+    description:
+      "La solución óptima para el balanceo de carga, alto rendimiento y alta disponibilidad.",
+    img: atLogic,
+    path: "/soluciones/at-logic",
+    cta: "Ver más",
+  },
+  {
+    title: "AT Drone",
+    description:
+      "Drones de alto rendimiento pensados para todo tipo de proyecto táctico o multimedia.",
+    img: atDrone,
+    path: "/soluciones/at-drone",
+    cta: "Ver más",
+  },
 ]
